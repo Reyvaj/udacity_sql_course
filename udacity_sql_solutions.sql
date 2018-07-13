95a0b579dd4af2c27baf87f38cd683c029b546ab
Javier M Ortiz <javier.ortiz989@gmail.com>
2018-07-13 12:07:59 -0700

    Add CASE solutions

--- udacity_sql_solutions.sql
+++ udacity_sql_solutions.sql
@@ -233,3 +233,81 @@ ON a.id = o.account_id
 WHERE a.name = 'Walmart'
 GROUP BY 1
 ORDER BY 2 DESC;
+
+'''
+We would like to understand 3 different levels of customers based
+on the amount associated with their purchases. The top branch includes
+anyone with a Lifetime Value (total sales of all orders) greater than
+200,000 usd. The second branch is between 200,000 and 100,000 usd. The
+lowest branch is anyone under 100,000 usd. Provide a table that includes
+the level associated with each account. You should provide the account name,
+the total sales of all orders for the customer, and the level. Order with
+the top spending customers listed first.
+'''
+SELECT a.id, a.name, SUM(o.total_amt_usd) as total_sales,
+CASE WHEN SUM(o.total_amt_usd)>200000 THEN 'top'
+     WHEN SUM(o.total_amt_usd)>100000 THEN 'middle'
+     ELSE 'low' END AS customer_level
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+GROUP BY a.id, a.name
+ORDER BY total_sales;
+
+'''
+We would now like to perform a similar calculation to the first,
+but we want to obtain the total amount spent by customers only in
+2016 and 2017. Keep the same levels as in the previous question.
+Order with the top spending customers listed first.
+'''
+SELECT a.id, a.name, SUM(o.total_amt_usd) AS total_sales,
+       CASE WHEN SUM(o.total_amt_usd)>200000 THEN 'top'
+            WHEN SUM(o.total_amt_usd)>100000 THEN 'middle'
+            ELSE 'low' END AS customer_level
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+WHERE o.occurred_at > '2015-12-31'
+GROUP BY a.id, a.name
+ORDER BY total_sales DESC;
+
+'''
+We would like to identify top performing sales reps, which are sales
+reps associated with more than 200 orders. Create a table with the sales
+rep name, the total number of orders, and a column with top or not
+depending on if they have more than 200 orders. Place the top sales
+people first in your final table.
+'''
+SELECT s.name, COUNT(*) AS total_orders,
+       CASE WHEN COUNT(*) > 200 THEN 'top'
+       ELSE 'bottom' END AS rep_level
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+JOIN sales_reps s
+ON a.sales_rep_id = s.id
+GROUP BY 1
+ORDER BY 2 DESC;
+
+"""
+The previous didn't account for the middle, nor the dollar amount associated
+with the sales. Management decides they want to see these characteristics
+represented as well. We would like to identify top performing sales reps,
+which are sales reps associated with more than 200 orders or more than
+750000 in total sales. The middle group has any rep with more than 150
+orders or 500000 in sales. Create a table with the sales rep name, the
+total number of orders, total sales across all orders, and a column with top,
+middle, or low depending on this criteria. Place the top sales people based on
+dollar amount of sales first in your final table.
+"""
+SELECT s.name, COUNT(*) AS total_orders, SUM(total_amt_usd) AS total_sales,
+       CASE WHEN COUNT(*) > 200 OR SUM(total_amt_usd) > 750000 THEN 'top'
+            WHEN COUNT(*) > 150 OR SUM(total_amt_usd) > 500000 THEN 'middle'
+            ELSE 'bottom' END AS sales_rep_level
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+JOIN sales_reps s
+ON a.sales_rep_id = s.id
+GROUP BY 1
+ORDER BY 3 DESC;
