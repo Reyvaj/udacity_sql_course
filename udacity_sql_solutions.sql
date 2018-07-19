12c6474dc68430f5e46c25366f71a3e79fadbd26
Javier M Ortiz <javier.ortiz989@gmail.com>
2018-07-19 10:06:02 -0700

    Add POSITION & STRPOS solutions
    Added solutions to two problems about POSITION & STRPOS SQL functions

--- udacity_sql_solutions.sql
+++ udacity_sql_solutions.sql
@@ -642,3 +642,21 @@ SELECT name, CASE WHEN LEFT(name, 1) in
              CASE WHEN LEFT(name, 1) in
                   ('a','e','i','o','u') THEN 0 ELSE 1 END everything_else
 FROM accounts) sub;
+
+/* POSITION & STRPOS */
+'''
+Use the accounts table to create first and last name columns 
+that hold the first and last names for the primary_poc. 
+'''
+SELECT LEFT(primary_poc, POSITION(' ' in primary_poc)) AS first_name, 
+       RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) 
+       AS last_name
+FROM accounts
+
+'''
+Now see if you can do the same thing for every rep name in the 
+sales_reps table. Again provide first and last name columns.
+'''
+SELECT LEFT(name, POSITION(' ' IN name)) AS first_name,
+       RIGHT(name, LENGTH(name)-POSITION(' ' IN name)) AS last_name
+FROM sales_reps
