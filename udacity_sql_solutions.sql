b2c84e779e53583ca9676b6727983c3fb299034e
Javier M Ortiz <javier.ortiz989@gmail.com>
2018-07-19 08:56:17 -0700

    Add LEFT & RIGHT solutions
    Solutions to 4 problems regarding LEFT & RIGHT SQL functions.

--- udacity_sql_solutions.sql
+++ udacity_sql_solutions.sql
@@ -588,3 +588,57 @@ WITH t1 AS (
   HAVING AVG(o.total_amt_usd) > (SELECT avg_all FROM t1))
 SELECT AVG(avg_spent)
 FROM t2;
+
+/* LEFT & RIGHT */
+'''
+In the accounts table, there is a column holding the
+website for each company. The last three digits specify
+what type of web address they are using. A list of extensions
+(and pricing) is provided here. Pull these extensions and
+provide how many of each website type exist in the accounts table.
+'''
+SELECT RIGHT(website, 4) AS extensions, COUNT(*)
+FROM accounts
+GROUP BY 1
+ORDER BY 2 DESC;
+
+'''
+There is much debate about how much the name (or even
+the first letter of a company name) matters. Use the
+accounts table to pull the first letter of each company
+name to see the distribution of company names that begin
+with each letter (or number).
+'''
+SELECT LEFT(UPPER(name), 1) AS first_letter, COUNT(*)
+FROM accounts
+GROUP BY 1
+ORDER BY 2 DESC;
+
+'''
+Use the accounts table and a CASE statement to create two groups:
+one group of company names that start with a number and a second
+group of those company names that start with a letter. What
+proportion of company names start with a letter?
+'''
+WITH t1 AS (
+      SELECT name, CASE WHEN LEFT(UPPER(name), 1) IN
+                ('1','2','3','4','5','6','7','8','9','0')
+            THEN 1 ELSE 0 END names_with_num,
+            CASE WHEN LEFT(UPPER(name), 1) IN
+                ('1','2','3','4','5','6','7','8','9','0')
+            THEN 0 ELSE 1 END names_with_letters
+      FROM accounts)
+SELECT SUM(names_with_num) AS nums, SUM(names_with_letters) AS letters
+FROM t1
+
+'''
+Consider vowels as a, e, i, o, and u. What proportion of company names
+start with a vowel, and what percent start with anything else?
+'''
+SELECT SUM(vowels) v, SUM(everything_else) e
+FROM (
+SELECT name, CASE WHEN LEFT(name, 1) in
+                  ('a','e','i','o','u') THEN 1 ELSE 0 END vowels,
+             CASE WHEN LEFT(name, 1) in
+                  ('a','e','i','o','u') THEN 0 ELSE 1 END everything_else
+FROM accounts) sub;
