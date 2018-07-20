8da446a78cd7ec5ff5f1ba47e77125cf27d75464
Javier M Ortiz <javier.ortiz989@gmail.com>
2018-07-20 12:55:30 -0700

    Add CONCAT, CAST, COALESCE solutions
    Solutions to the problems related to functions CONCAT, CAST, COALESCE

--- udacity_sql_solutions.sql
+++ udacity_sql_solutions.sql
@@ -660,3 +660,75 @@ sales_reps table. Again provide first and last name columns.
 SELECT LEFT(name, POSITION(' ' IN name)) AS first_name,
        RIGHT(name, LENGTH(name)-POSITION(' ' IN name)) AS last_name
 FROM sales_reps
+
+/*CONCAT*/
+'''
+Each company in the accounts table wants to create an email address
+for each primary_poc. The email address should be the first name of
+the primary_poc . last name primary_poc @ company name .com.
+'''
+WITH t1 AS(
+  SELECT lower(name) AS name,
+        LEFT(lower(primary_poc), POSITION(' ' in primary_poc)-1) AS first_name,
+        RIGHT(lower(primary_poc), LENGTH(primary_poc)-POSITION(' ' in primary_poc)) AS last_name
+  FROM accounts)
+SELECT CONCAT(CONCAT(CONCAT(first_name, last_name), '@'), name)
+FROM t1;
+
+'''
+You may have noticed that in the previous solution some of the
+company names include spaces, which will certainly not work in an
+email address. See if you can create an email address that will
+work by removing all of the spaces in the account name, but
+otherwise your solution should be just as in question 1.
+'''
+WITH t1 AS(
+  SELECT lower(replace(name, ' ', '')) AS name,
+        LEFT(lower(primary_poc), POSITION(' ' in primary_poc)-1) AS first_name,
+        RIGHT(lower(primary_poc), LENGTH(primary_poc)-POSITION(' ' in primary_poc)) AS last_name
+  FROM accounts)
+SELECT CONCAT(CONCAT(CONCAT(first_name, last_name), '@'), name)
+FROM t1;
+
+"""
+We would also like to create an initial password, which they will
+change after their first log in. The first password will be the first
+letter of the primary_poc's first name (lowercase), then the last
+letter of their first name (lowercase), the first letter of their
+last name (lowercase), the last letter of their last name (lowercase),
+the number of letters in their first name, the number of letters in
+their last name, and then the name of the company they are working
+with, all capitalized with no spaces.
+"""
+WITH t1 AS(
+  SELECT lower(replace(name, ' ', '')) AS company,
+        LEFT(lower(primary_poc), POSITION(' ' in primary_poc)-1) AS first_name,
+        RIGHT(lower(primary_poc), LENGTH(primary_poc)-POSITION(' ' in primary_poc)) AS last_name
+  FROM accounts)
+SELECT UPPER(LEFT(first_name, 1) || RIGHT(first_name, 1) || LEFT(last_name, 1) ||
+        RIGHT(last_name, 1) || LENGTH(first_name) || LENGTH(last_name) ||
+        company)
+FROM t1
+
+'''
+Write a query to change the date into the correct SQL date format.
+'''
+WITH t1 AS (
+      SELECT SUBSTR(date, 1, 2) AS month, SUBSTR(date, 4, 2) AS day,
+              SUBSTR(date, 7, 4) AS year
+      FROM sf_crime_data)
+SELECT (year || month || day)::DATE AS sql_date_format
+FROM t1;
+
+/*COALESCE*/
+SELECT COALESCE(a.id, a.id) filled_id, a.name, a.website, a.lat, a.long, a.primary_poc, a.sales_rep_id, COALESCE(o.account_id, a.id) account_id, COALESCE(o.standard_qty, 0) standard_qty, COALESCE(o.gloss_qty, 0) gloss_qty,
+COALESCE(o.poster_qty, 0) poster_qty,
+COALESCE(o.total, 0) total,
+COALESCE(o.standard_amt_usd, 0) standard_amt_usd,
+COALESCE(o.gloss_amt_usd, 0) gloss_amt_usd,
+COALESCE(o.poster_amt_usd, 0) poster_amt_usd,
+COALESCE(o.total_amt_usd, 0) total_amt_usd
+FROM accounts a
+LEFT JOIN orders o
+ON a.id = o.account_id
+WHERE o.total IS NULL;
